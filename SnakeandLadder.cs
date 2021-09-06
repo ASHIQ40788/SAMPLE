9f881fec80d889f008a625e6dce40f5a93bcb686
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-06 17:31:07 +0530

    [ASHIQ] added:DIE_NUMBER

--- SnakeandLadder.cs
+++ SnakeandLadder.cs
@@ -1,27 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SnakeLadder
 {
-    public class Player1
-    {
-        static void Main(string[] args)
-        {
-            // one player starting on square one
-            int[] players = {1};
-            while (true)
-            {
-                for (int i = 0; i < players.Length; i++)
-                {
-                    int ns = Turn(i + 1, players[i]);
-                    if (ns == 100)
-                    {
-                        Console.WriteLine("Player {0} wins!", i + 1);
-                        return;
-                    }
-                    players[i] = ns;
-                    Console.WriteLine();
-                }
-            }
-        }
-    }
+	class SnakeandLadder
+	{
+		static void Main(string[] args)
+		{
+			Random rnd = new Random();
+			int dice = rnd.Next(1, 7);// creates a number between 1 and 6
+			Console.Write(dice);
+			Console.Read();
+		}
+	}
 }
