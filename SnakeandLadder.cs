b2c2a20a6c38a468d7bc345a7b9e8422969ce2af
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-06 21:26:07 +0530

    [ASHIQ] added : winning_position

--- SnakeandLadder.cs
+++ SnakeandLadder.cs
@@ -1,44 +1,86 @@
-﻿// C# program to find minimum
-// number of dice throws required
-// to reach last cell from first
-// cell of a given snake and ladder board
-using System;
+﻿using System;
 using System.Collections.Generic;
 
-public class SnakesLadder
+namespace SnakeAndLadder
 {
-    /public class Snakes_and_Ladders
-    {// start class
-        //Main Method
-        public static void main(String[] args) throws IOException
-        {// start main method
+    class Program
+    {
+        private static Dictionary<int, int> snl = new Dictionary<int, int>() {
+            {4, 14},
+            {9, 31},
+            {17, 7},
+            {20, 38},
+            {28, 84},
+            {40, 59},
+            {51, 67},
+            {54, 34},
+            {62, 19},
+            {63, 81},
+            {64, 60},
+            {71, 91},
+            {87, 24},
+            {93, 73},
+            {95, 75},
+            {99, 78},
+        };
+        private static Random rand = new Random();
+        private const bool sixesThrowAgain = true;
 
-            //Welcoming Screen
-            Console.WriteLine ("\t\t\t\t\t\tWelcome To Snakes And Ladders\n\n");
-            while (counter > 0)
+        static int Turn(int player, int square)
+        {
+            while (true)
             {
-            if (counter%10 == 0 && counter != 100)
-            {
-            if(iteration==-1)
-            {
-             // subtract 9 from the counter
-                    // and sets it to start adding by one
-                    counter-=9;
-                    iteration=1;
-            }
+                int roll = rand.Next(1, 6);
+                Console.Write("Player {0}, on square {1}, rolls a {2}", player, square, roll);
+                if (square + roll > 100)
+                {
+                    Console.WriteLine(" but cannot move.");
+                }
                 else
                 {
-                    System.out.print(counter+"\t");
-                     counter-=10;
-                    iteration=-1; // set the counter to start subtract by ones
-                 }
-                if (counter != 0)
-                    { 
-                   System.out.print("\n" + counter + "\t");
-                 System.out.print(counter + "\t"); // just prints out the counter
-                counter += iteration; // sets counter to add by iteration
-                  }// end while
-              }
+                    square += roll;
+                    Console.WriteLine(" and moves to square {0}", square);
+                    if (square == 100) return 100;
+                    int next = square;
+                    if (snl.ContainsKey(square))
+                    {
+                        next = snl[square];
+                    }
+                    if (square < next)
+                    {
+                        Console.WriteLine("Yay! Landed on a ladder. Climb up to {0}.", next);
+                        if (next == 100) return 100;
+                        square = next;
+                    }
+                    else if (square > next)
+                    {
+                        Console.WriteLine("Oops! Landed on a snake. Slither down to {0}.", next);
+                    }
+                }
+                if (roll < 6 || !sixesThrowAgain) return square;
+                Console.WriteLine("Rolled a 6 so roll again.");
             }
-       }
- }
+        }
+
+        static void Main(string[] args)
+        {
+            // three players atarting on square one
+            int[] players = { 1, 1, 1 };
+            while (true)
+            {
+                for (int i = 0; i < players.Length; i++)
+                {
+                    int ns = Turn(i + 1, players[i]);
+                    if (ns == 100)
+                    {
+                        Console.WriteLine("Player {0} wins!", i + 1);
+                        return;
+                    }
+                    players[i] = ns;
+                    Console.WriteLine();
+                    Console.Read();
+                }
+            }
+        }
+    }
+}
